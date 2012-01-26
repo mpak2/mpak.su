48f8c20d696cc9fc096e03ab97eafd59bb4b8b6a
Евгений Кривошлыков <github.com@mpak.su>
2012-01-26 18:40:01 +0300

    2012.01.26 18:40:01

--- modules/onpay/new.новый.php
+++ modules/onpay/new.новый.php
@@ -3,7 +3,9 @@
 if($_POST['sum']){
 	$mpdbf = mpdbf($tn = "{$conf['db']['prefix']}{$arg['modpath']}_operations", $_POST);
 	mpqw($sql = "INSERT INTO $tn SET $mpdbf, date=NOW(), type=\"". ($_GET['type'] ? $_GET['type'] : $arg['modpath'])."\", uid=".(int)$conf['user']['uid']);
-	header("Location: /{$arg['modpath']}:new/". mysql_insert_id());
+	if($operations_id = mysql_insert_id()){
+		mpevent("Создание нового платежа", $operations_id, $conf['user']['uid']);
+	} header("Location: /{$arg['modpath']}:new/". mysql_insert_id());
 }elseif($_GET['id']){
 	$conf['tpl']['operation'] = mpql(mpqw($sql = "SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_operations".($_GET['id'] ? " WHERE id=".(int)$_GET['id'] : '')." ORDER BY id DESC LIMIT 1"), 0);
 }
