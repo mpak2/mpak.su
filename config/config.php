2fdec3abd997549d7ee99555d356bedf9bba887f
Евгений Кривошлыков <github.com@mpak.su>
2012-02-01 15:20:02 +0300

    2012.02.01 15:20:01

--- config/config.php
+++ config/config.php
@@ -2,6 +2,7 @@
 
 mb_internal_encoding("UTF-8");
 date_default_timezone_set('Europe/Moscow');
+//$conf['fs']['path'] = dirname(dirname(__FILE__));
 $conf['fs']['path'] = "../".(substr($_SERVER['SERVER_NAME'], 0, 4) != 'www.' ? 'www.' : '')."{$_SERVER['SERVER_NAME']}:../mpak.cms";
 $conf['db']['conn'] = null;
 $conf['db']['type'] = 'mysql';
