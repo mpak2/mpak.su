efe3b65f81adc135da1dea35be65f3b9ae852d10
Евгений Кривошлыков <github.com@mpak.su>
2012-02-07 02:00:01 +0300

    2012.02.07 02:00:01

--- modules/pages/index.php
+++ modules/pages/index.php
@@ -22,9 +22,11 @@ if($_GET['pid']){
 }elseif($arg['access'] >= 4 && is_numeric($_GET['del'])){
 	mpqw("DELETE FROM {$conf['db']['prefix']}{$arg['modpath']}_index WHERE id=".(int)$_GET['del']." LIMIT 1");
 }elseif($arg['access'] >= 4 && isset($_GET['new'])){
-	if(max($_GET['id'], $_GET['pid'])){
-		mpqw("INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_index SET id=".(int)max($_GET['id'], $_GET['pid']). ", uid=".(int)$conf['user']['uid']. ", date=". time().", title=\"Заголовок страницы\", text=\"Текст страницы\"");
-		header("Location: /?m[{$arg['modpath']}]=admin&r={$conf['db']['prefix']}{$arg['modpath']}_index&edit=".(int)max($_GET['id'], $_GET['pid']));
+	if($max = max($_GET['id'], $_GET['pid'])){
+		mpqw("INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_index SET id=".(int)$max. ", uid=".(int)$conf['user']['uid']. ", time=". time().", name=\"Заголовок страницы\", text=\"Текст страницы\"");
+		if($id = mysql_insert_id()){
+			header("Location: /?m[{$arg['modpath']}]=admin&r={$conf['db']['prefix']}{$arg['modpath']}_index&where[id]=". (int)$id. "&edit=".(int)$id);
+		}
 	}else{
 		header("HTTP/1.1 404 Not Found");
 	}
--- modules/users/default.tpl
+++ modules/users/default.tpl
@@ -8,8 +8,8 @@
 	</script>
 	<div id="block_users">
 		<div>
-			<div style="width:50%; float:left;"><!-- [blocks:5] --></div>
-			<div style="width:50%; margin-left;"><!-- [blocks:6] --></div>
+			<div style="width:50%; float:left;" ><!-- [blocks:5] --></div>
+			<div style="width:50%; margin-left:50%;" ><!-- [blocks:6] --></div>
 		</div>
 	</div>
 </div>
