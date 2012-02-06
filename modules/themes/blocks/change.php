35dfbc352a06d8713c5021b71e73f9c09bd61230
Евгений Кривошлыков <github.com@mpak.su>
2012-02-06 14:20:01 +0300

    2012.02.06 14:20:01

---
+++ modules/themes/blocks/change.php
@@ -0,0 +1,73 @@
+<? die; # Нуль
+
+if ((int)$arg['confnum']){
+	$param = unserialize(mpql(mpqw("SELECT param FROM {$conf['db']['prefix']}blocks WHERE id = {$arg['confnum']}"), 0, 'param'));
+	if ($_POST){
+		$param = array($_POST['param']=>$_POST['val'])+(array)$param;
+		mpqw("UPDATE {$conf['db']['prefix']}blocks SET param = '".serialize($param)."' WHERE id = {$arg['confnum']}");
+	} if(array_key_exists("null", $_GET)) exit;
+
+	$klesh = array(
+/*		"Количество символов"=>0,
+		"Курс доллара"=>30,
+		"Список"=>array(
+			1=>"Одын",
+			2=>"Два",
+		),
+		"Город"=>spisok("SELECT id, name FROM {$conf['db']['prefix']}users_sity ORDER BY name"),*/
+	);
+
+?>
+		<!-- Настройки блока -->
+	<script src="/include/jquery/my/jquery.klesh.select.js"></script>
+	<script>
+		$(function(){
+			<? foreach($klesh as $k=>$v): ?>
+				<? if(gettype($v) == 'array'): ?>
+					$(".klesh_<?=strtr(md5($k), array("="=>''))?>").klesh("/?m[blocks]=admin&r=mp_blocks&null&conf=<?=$arg['confnum']?>", function(){
+					}, <?=json_encode($v)?>);
+				<? else: ?>
+					$(".klesh_<?=strtr(md5($k), array("="=>''))?>").klesh("/?m[blocks]=admin&r=mp_blocks&null&conf=<?=$arg['confnum']?>");
+				<? endif; ?>
+			<? endforeach; ?>
+		});
+	</script>
+	<div style="margin-top:10px;">
+		<? foreach($klesh as $k=>$v): ?>
+			<div style="overflow:hidden;">
+				<div style="width:200px; float:left; padding:5px; text-align:right; font-weight:bold;"><?=$k?> :</div>
+				<? if(gettype($v) == 'array'): ?>
+					<div class="klesh_<?=strtr(md5($k), array("="=>''))?>" param="<?=$k?>"><?=$v[ $param[$k] ]?></div>
+				<? else: ?>
+					<div class="klesh_<?=strtr(md5($k), array("="=>''))?>" param="<?=$k?>"><?=($param[$k] ?: $v)?></div>
+				<? endif; ?>
+			</div>
+		<? endforeach; ?>
+	</div>
+<? return;
+
+}//$param = unserialize(mpql(mpqw("SELECT param FROM {$conf['db']['prefix']}blocks WHERE id = {$arg['blocknum']}"), 0, 'param'));
+//$uid = $_GET['id'] && array_key_exists('users', $_GET['m']) ? $_GET['id'] : $conf['user']['id'];
+//if(array_key_exists('blocks', $_GET['m']) && array_key_exists('null', $_GET) && ($_GET['id'] == $arg['blocknum']) && $_POST){};
+
+//$dat = mpql(mpqw("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_{$arg['fn']} LIMIT 10"));
+$themes = mpreaddir("themes", 1);// mpre($themes);
+
+?>
+<script>
+	$(function(){
+		$(".themes_<?=$arg['modpath']?> select").change(function(){
+			theme = $(this).find("option:selected").val();// alert(theme);
+			$.post("/blocks/<?=$arg['blocknum']?>/null", {theme:theme}, function(data){
+				if(isNaN(data){ alert(data) }
+			});
+		});
+	});
+</script>
+<div class="themes_<?=$arg['modpath']?>">
+	<select>
+		<? foreach($themes as $k=>$v): ?>
+			<option <?=($conf['settings']['theme'] == $v ? "selected" : "")?>><?=$v?></option>
+		<? endforeach; ?>
+	</select>
+</div>
