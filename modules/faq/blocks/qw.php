a882396fc6b12735ca84043849d71ff1a898366e
Евгений Кривошлыков <github.com@mpak.su>
2012-02-12 21:20:03 +0300

    2012.02.12 21:20:01

--- modules/faq/blocks/qw.php
+++ modules/faq/blocks/qw.php
@@ -13,11 +13,13 @@ EOF;*/
 	return;
 }//$param = unserialize(mpql(mpqw("SELECT param FROM {$conf['db']['prefix']}blocks WHERE id = {$arg['blocknum']}"), 0, 'param'));
 //$uid = $_GET['id'] && array_key_exists('users', $_GET['m']) ? $_GET['id'] : $conf['user']['id'];
-if(($arg['access'] > 1) && array_key_exists('blocks', $_GET['m']) && array_key_exists('null', $_GET) && ($_GET['id'] == $arg['blocknum']) && $_POST){
-	if($_POST['del'] && $arg['access'] > 2){
+if(array_key_exists('blocks', $_GET['m']) && array_key_exists('null', $_GET) && ($_GET['id'] == $arg['blocknum']) && $_POST){
+	if($arg['access'] <= 1){
+		echo "Доступ только для чтения"; exit;
+	}elseif($_POST['del'] && $arg['access'] > 2){
 		mpqw("DELETE FROM {$conf['db']['prefix']}{$arg['modpath']}_index WHERE uid=". (int)$conf['user']['uid']. " AND id=". (int)$_POST['del']); exit;
 	}else if($mpdbf = mpdbf($tn = "{$conf['db']['prefix']}{$arg['modpath']}_index", $post = (array('time'=>time(), 'uid'=>$conf['user']['uid'])+$_POST))){
-		mpqw("INSERT INTO $tn SET $mpdbf");
+		mpqw($sql = "INSERT INTO $tn SET $mpdbf"); echo $sql;
 		$data = array(array('id'=>mysql_insert_id())+$post);
 	}
 }else{
@@ -33,9 +35,9 @@ if(($arg['access'] > 1) && array_key_exists('blocks', $_GET['m']) && array_key_e
 		$(function(){
 			$(".dat_<?=$arg['blocknum']?> form").iframePostForm({
 				complete:function (data){
-					if(isNaN(data)){ alert(data); }else{
-						text = $("<div>").append(data).find(".data_<?=$arg['blocknum']?>").html();
-//						$(".data_<?=$arg['blocknum']?>").prepend(text);
+					if(isNaN(data)){// alert(data); }else{
+						text = $("<div>").append(data).find(".data_<?=$arg['blocknum']?>").html();// alert(text);
+						$(".data_<?=$arg['blocknum']?>").prepend(text);
 					}
 				}
 			})//.find("textarea").text('');
