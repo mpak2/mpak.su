86817267a2087b860193c05172b0b2263404f34d
Евгений Кривошлыков <github.com@mpak.su>
2012-02-12 11:00:02 +0300

    2012.02.12 11:00:01

--- modules/backup/info.php
+++ modules/backup/info.php
@@ -7,7 +7,7 @@ $conf['modversion']=array(
 	'version'=>'0,9006',
 	'description'=>'Создание и откат резервных копий',
 	'enabled'=>'2',
-	'access'=>'1',
+	'access'=>'5',
 	'admin'=>'4',
 	'md5'=>'1e21159b34a9e0494e4caf4804b94087',
 );
