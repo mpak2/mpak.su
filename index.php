4650236f6c9ed243ee118941a37267d68384661e
mpak <git@mpak.su>
2012-03-15 23:00:44 +0000

    2012.03.15 23:00:01

--- index.php
+++ index.php
@@ -118,6 +118,8 @@ if ($conf['settings']['del_sess'] && ($conf['settings']['del_sess'] != 3 || $_SE
 //}
 
 if (strlen($_POST['name']) && strlen($_POST['pass']) && $_POST['reg'] == 'Аутентификация' && $uid = mpql(mpqw("SELECT id FROM {$conf['db']['prefix']}users WHERE tid=1 AND name = \"".mpquot($_POST['name'])."\" AND pass='".mphash($_POST['name'], $_POST['pass'])."'", 'Проверка существования пользователя'), 0, 'id')){# Авторизация пользователя
+	print_r($sess);
+	print_r($_POST); exit;
 	mpqw($sql = "UPDATE {$conf['db']['prefix']}sess SET uid=".($sess['uid'] = $uid)." WHERE id=". (int)$sess['id']);// echo $sql;
 	mpqw("UPDATE {$conf['db']['prefix']}users SET last_time=". time(). " WHERE id=".(int)$uid);
 	header("Location: ". $_SERVER['REQUEST_URI']); exit;
