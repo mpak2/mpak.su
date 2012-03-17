841661ff1d6a837d64d6813d5adf3cdefc8f7c91
mpak <git@mpak.su>
2012-03-17 20:20:03 +0000

    2012.03.17 20:20:01

--- modules/menu/admin.php
+++ modules/menu/admin.php
@@ -104,7 +104,7 @@ if ($_GET['r'] == "{$conf['db']['prefix']}{$arg['modpath']}_region"){ // 'Мен
 				'img'=>array('*'=>"<img src='/{$arg['modpath']}:img/{f:id}/w:100/h:80/null/img.jpg'>"),
 				'pid'=>array('*'=>"<a href=/?m[menu]=admin&r=mp_menu&where[rid]={f:rid}&where[pid]=0>{spisok:{f}}</a>"),
 //				'count'=>spisok("SELECT m1.id, COUNT(*) FROM {$conf['db']['prefix']}{$arg['modpath']} AS m1, {$conf['db']['prefix']}{$arg['modpath']} AS m2 WHERE m1.id=m2.pid GROUP BY m1.id"),
-				(($fn = 'menu'). ($prx = ''))=>array('*'=>"<a href=/?m[{$arg['modpath']}]=admin&r={$conf['db']['prefix']}{$arg['modpath']}&where[pid]={f:id}&where[rid]={f:rid}>Нет</a>")+spisok("SELECT r.id, CONCAT('<a href=/?m[{$arg['modpath']}]=admin&r={$conf['db']['prefix']}{$arg['modpath']}&where[pid]=', r.id, '>', COUNT(*), '_{$fn}</a>') FROM {$_GET['r']} AS r, {$conf['db']['prefix']}{$arg['modpath']} AS fn WHERE r.id=fn.pid GROUP BY r.id"),
+				(($fn = 'menu'). ($prx = ''))=>array('*'=>"<a href=/?m[{$arg['modpath']}]=admin&r={$conf['db']['prefix']}{$arg['modpath']}&where[pid]={f:id}&where[rid]={f:rid}>Нет</a>")+spisok("SELECT r.id, CONCAT('<a href=/?m[{$arg['modpath']}]=admin&r={$conf['db']['prefix']}{$arg['modpath']}&where[pid]=', r.id, '&where[rid]=', r.rid, '>', COUNT(*), '_{$fn}</a>') FROM {$_GET['r']} AS r, {$conf['db']['prefix']}{$arg['modpath']} AS fn WHERE r.id=fn.pid GROUP BY r.id"),
 			), # Шаблон вывода в замене участвуют только поля запроса имеен приоритет перед полем set
 //			'disable' => array('orderby'), # Выключенные для записи поля
 //			'hidden' => array('name', 'enabled'), # Скрытые поля
