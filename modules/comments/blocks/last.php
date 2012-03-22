60ea72c11da91f2364bcf4e687e69b8aac79dc46
mpak <git@mpak.su>
2012-03-22 11:40:06 +0000

    2012.03.22 11:40:01

--- modules/comments/blocks/last.php
+++ modules/comments/blocks/last.php
@@ -12,18 +12,18 @@ if ((int)$arg['confnum']){
 //$comments = mpql(mpqw("SELECT t.*, u.url, SUBSTR(t.text, 1, 80) as text FROM {$conf['db']['prefix']}{$arg['modpath']}_url AS u, {$conf['db']['prefix']}{$arg['modpath']}_txt AS t WHERE u.id=t.uid ORDER BY id DESC LIMIT 10"));
 //include(mpopendir("modules/{$arg['modpath']}/blocks/last.tpl"));
 
-$comments = mpqn(mpqw("SELECT t.* FROM {$conf['db']['prefix']}{$arg['modpath']}_txt AS t LEFT JOIN {$conf['db']['prefix']}{$arg['modpath']}_url AS u ON t.uid=u.id"));
+$comments = mpqn(mpqw("SELECT t.*, url.name AS url_name FROM {$conf['db']['prefix']}{$arg['modpath']}_txt AS t LEFT JOIN {$conf['db']['prefix']}{$arg['modpath']}_url AS url ON t.url_id=url.id"));
 
 ?>
 <div>
+	<div style="margin-bottom:5px;">
+		Последние / <a href="/<?=$arg['modname']?>">Все</a>
+	</div>
 	<? foreach($comments as $k=>$v): ?>
 		<div>
 			<div style="float:right"><?=date('d.m.Y H:i:s', $v['time'])?></div>
 			<div><a href="/<?=$conf['modules']['users']['modname']?>/<?=$v['uid']?>"><?=$v['uname']?></a></div>
-			<div><?=$v['text']?></div>
+			<div><a href="<?=$v['uname']?>"><?=$v['text']?></a></div>
 		</div>
 	<? endforeach; ?>
-	<div style="text-align:right;">
-		<a href="/<?=$arg['modname']?>">Все комментарии</a>
-	</div>
 </div>
