e21a8c33875daf1cfc373352dba680d36124a79c
mpak <git@mpak.su>
2012-03-27 07:40:02 +0000

    2012.03.27 07:40:01

--- modules/null/.captcha.php
+++ modules/null/.captcha.php
@@ -2,8 +2,8 @@
 
 ini_set('display_errors','Off');
 include "include/kcaptcha/index.php";
+
 $captcha = new KCAPTCHA();
 setcookie("captcha_keystring", md5($captcha->getKeyString()), 0, '/');
-exit;
 
 ?>
