77f1e6b4f9e66f7aa4a4759a0ea7009519c0f9ab
mpak <git@mpak.su>
2012-04-06 09:40:02 +0000

    2012.04.06 09:40:01

--- modules/null/admin_interface.php
+++ modules/null/admin_interface.php
@@ -1,6 +1,6 @@
 <? die;
 
 $tn = $conf['db']['prefix']. $arg['modpath']. substr($arg['fn'], strlen("admin_interface"));
-header("location: /?m[users]=admin&r=". $tn); exit;
+header("location: /?m[{$arg['modpath']}]=admin&r=". $tn); exit;
 
 ?>
