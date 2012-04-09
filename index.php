ae2e43c8d6ceaa9a09bb8db52ccfd0cefbdccf0e
mpak <git@mpak.su>
2012-04-09 14:20:02 +0000

    2012.04.09 14:20:01

--- index.php
+++ index.php
@@ -264,9 +264,11 @@ if (!function_exists('mcont')){
 				if (file_exists(mpopendir("modules/{$mod['folder']}/deny.php"))){
 					$content = mpct("modules/{$mod['folder']}/deny.php", $conf['arg'] = array('modpath'=>$mod['folder']));
 				}else if(!array_key_exists("themes", $_GET)){
-//					header('HTTP/1.0 404 Unauthorized');
+					if($_SERVER['REQUEST_URI'] != "/admin"){
+						header('HTTP/1.0 404 Unauthorized');
 //					echo "<div style='margin:100px 0;text-align:center'>Доступ запрещен</div>";
-//					header("Location: /themes:404". $_SERVER['REQUEST_URI']);
+						header("Location: /admin");
+					}
 				}
 			}
 		}
