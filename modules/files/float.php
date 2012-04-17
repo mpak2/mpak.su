bbed51734a2b77f72b13411c827d025d194f4ac9
mpak <git@mpak.su>
2012-04-17 20:00:01 +0000

    2012.04.17 20:00:01

--- modules/files/float.php
+++ modules/files/float.php
@@ -6,7 +6,10 @@ if(array_key_exists("null", $_GET) && $_FILES){
 			null, array("time"=>time(), "uid"=>$conf['user']['uid'])
 		);
 		if($fn = mpfn($tn, $k, $doc_id, "img")){
-			mpqw("UPDATE $tn SET name=\"". mpquot($_FILES['img']['name'][$k]). "\", doc=\"". mpquot($fn). "\" WHERE id=". (int)$doc_id);
+			mpfdk($tn,
+				array("id"=>$doc_id), $w = array("name"=>$_FILES['img']['name'][$k], "img"=>$fn), $w
+			);
+//			mpqw("UPDATE $tn SET name=\"". mpquot($_FILES['img']['name'][$k]). "\", doc=\"". mpquot($fn). "\" WHERE id=". (int)$doc_id);
 		}else{
 			mpqw("DELETE FROM $tn WHERE id=". (int)$doc_id);
 		} 
