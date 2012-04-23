0c9ee529c2cbf4b575f9c03b74f4e936e5fd673a
mpak <git@mpak.su>
2012-04-23 08:00:02 +0000

    2012.04.23 08:00:01

--- modules/develop/index.php
+++ modules/develop/index.php
@@ -1,34 +1,30 @@
 <? die;
 
-$conf['tpl']['cat'] = spisok("SELECT id, name FROM {$conf['db']['prefix']}{$arg['modpath']}_kat ORDER BY sort DESC")+array(0=>"Новые");
+$conf['tpl']['cat'] = spisok("SELECT id, name FROM {$conf['db']['prefix']}{$arg['modpath']}_kat ORDER BY sort DESC")+array('0'=>'Новое');
 
-if(array_key_exists("null", $_GET)){
-	if($_POST['class'] == "klesh"){
-		if($arg['access'] >= 3){
-			if($plan = mpql(mpqw("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_plan WHERE id=". (int)$_POST['plan_id']), 0)){
-				mpevent("Изменение статуса задач разработки", $plan['name'], $plan['uid']);
-				mpqw("UPDATE {$conf['db']['prefix']}{$arg['modpath']}_plan SET kid=". (int)$_POST['val']. " WHERE id=". (int)$_POST['plan_id']);
-			}
-		}else{
-			exit("Доступ запрещен");
-		} exit($_POST['plan_id']);
-	}elseif($_POST['text'] && array_key_exists('null', $_GET)){
-		$_POST['text'] = preg_replace( '/(?<!S)((http(s?):\/\/)|(www\.[A-Za-zА-Яа-яЁё0-9-_]+\.))+([A-Za-zА-Яа-яЁё0-9\/*+-_?&;:%=.,#]+)/u', '<a href="http$3://$4$5" target="_blank" rel="nofollow">http$3://$4$5</a>', htmlspecialchars($_POST['text']));
-		$_POST['text'] = preg_replace ( '/(?<!S)([A-Za-zА-Яа-яЁё0-9_.\-]+\@{1}[A-Za-zА-Яа-яЁё0-9\.|-|_]*[.]{1}[a-z-а-я]{2,5})/u', '<a href="mailto:$1">$1</a>', $_POST['text'] );
-		mpqw("INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_work SET time=". time(). ", plan_id=". (int)$_POST['plan_id']. ", uid=". (int)$conf['user']['uid']. ", description=\"". mpquot($_POST['text']). "\"");
-		echo json_encode(array('time'=>date('Y.m.d H:i:d'), 'name'=>$conf['user']['uname'], 'text'=>$_POST['text']));
-		exit;
-	}elseif($_POST['submit'] && !empty($_POST['plan'])){
-		$max = mpql(mpqw("SELECT MAX(sort)+1 AS max FROM {$conf['db']['prefix']}{$arg['modpath']}_plan"), 0, 'max');
-		mpqw($sql = "INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_plan SET uid=".(int)$conf['user']['uid'].", time=".time().", plan=\"".($arg['access'] >= 4 ? mpquot($_POST['plan']) : htmlspecialchars(mpquot($_POST['plan'])))."\", sort=".(int)$max);
-		mpevent("Постановка новой задачи", "http://". mpidn($_SERVER['HTTP_HOST']). "/{$arg['modname']}/". mysql_insert_id(), $conf['user']['uid']);
-		header("Location: ". $_SERVER['REQUEST_URI']);
-	}elseif($_POST['golos']){
-		mpevent("Голосование за задачу", $_POST['golos']);
-		mpqw("INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_golos SET time=". time(). ", pid=". (int)$_POST['golos']. ", sid=". (int)$conf['user']['sess']['id']. " ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)");
-		mpqw("UPDATE {$conf['db']['prefix']}{$arg['modpath']}_plan SET time=". time(). " WHERE id=". (int)$_POST['golos']);
-		exit($_POST['golos']);
-	}
+if($_POST['text'] && array_key_exists('null', $_GET)){
+	$_POST['text'] = preg_replace( '/(?<!S)((http(s?):\/\/)|(www\.[A-Za-zА-Яа-яЁё0-9-_]+\.))+([A-Za-zА-Яа-яЁё0-9\/*+-_?&;:%=.,#]+)/u', '<a href="http$3://$4$5" target="_blank" rel="nofollow">http$3://$4$5</a>', htmlspecialchars($_POST['text']));
+	$_POST['text'] = preg_replace ( '/(?<!S)([A-Za-zА-Яа-яЁё0-9_.\-]+\@{1}[A-Za-zА-Яа-яЁё0-9\.|-|_]*[.]{1}[a-z-а-я]{2,5})/u', '<a href="mailto:$1">$1</a>', $_POST['text'] );
+	mpqw("INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_work SET time=". time(). ", plan_id=". (int)$_POST['plan_id']. ", uid=". (int)$conf['user']['uid']. ", description=\"". mpquot($_POST['text']). "\"");
+	echo json_encode(array('time'=>date('Y.m.d H:i:d'), 'name'=>$conf['user']['uname'], 'text'=>$_POST['text']));
+	exit;
+}elseif($_POST['submit'] && !empty($_POST['plan'])){
+	$max = mpql(mpqw("SELECT MAX(sort)+1 AS max FROM {$conf['db']['prefix']}{$arg['modpath']}_plan"), 0, 'max');
+	mpqw($sql = "INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_plan SET uid=".(int)$conf['user']['uid'].", time=".time().", plan=\"".($arg['access'] >= 4 ? mpquot($_POST['plan']) : htmlspecialchars(mpquot($_POST['plan'])))."\", sort=".(int)$max);
+	mpevent("Постановка новой задачи", "http://". mpidn($_SERVER['HTTP_HOST']). "/{$arg['modname']}/". mysql_insert_id(), $conf['user']['uid']);
+	header("Location: ". $_SERVER['REQUEST_URI']);
+}elseif(($arg['access'] >= 5) && array_key_exists('value', $_POST) && $_POST['id']){
+	if($plan = mpql(mpqw("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_plan WHERE id=". (int)$_POST['id']), 0)){
+		mpevent("Изменение статуса задачи", $_POST['id'], $plan['uid']);
+		mpqw($sql = "UPDATE {$conf['db']['prefix']}{$arg['modpath']}_plan SET time=". time(). ", kid=". (int)$_POST['value']. " WHERE id=". (int)$plan['id']);
+	} if(!mysql_error()){
+		echo $conf['tpl']['cat'][ $_POST['value'] ];
+	} exit;
+}elseif($_POST['golos']){
+	mpevent("Голосование за задачу", $_POST['golos']);
+	mpqw("INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_golos SET time=". time(). ", pid=". (int)$_POST['golos']. ", sid=". (int)$conf['user']['sess']['id']);
+	mpqw("UPDATE {$conf['db']['prefix']}{$arg['modpath']}_plan SET time=". time(). " WHERE id=". (int)$_POST['golos']);
+	echo $_POST['golos']; exit;
 }
 
 $conf['tpl']['cc'] = spisok("SELECT kid, COUNT(*) FROM {$conf['db']['prefix']}{$arg['modpath']}_plan GROUP BY kid");
