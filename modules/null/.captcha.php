62313f82faad2b7b29be681ee970b8bdb7ea353d
mpak <git@mpak.su>
2012-05-03 11:24:47 +0000

    Фиксируем изменения перед бурей

--- modules/null/.captcha.php
+++
@@ -1,9 +0,0 @@
-<? die;
-
-ini_set('display_errors','Off');
-include "include/kcaptcha/index.php";
-
-$captcha = new KCAPTCHA();
-setcookie("captcha_keystring", md5($captcha->getKeyString()), 0, '/');
-
-?>
