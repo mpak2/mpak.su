397a0eff5cdd2505f8702314cad8b6021bfdaff3
mpak <github.com@mpak.su>
2013-08-02 13:40:12 +0000

    Fri Aug  2 13:40:12 UTC 2013

--- modules/null/_img.php
+++ modules/null/_img.php
@@ -3,14 +3,13 @@
 if($_GET['tn']){
 	if(($i = $conf['settings'][$s = "{$arg['modpath']}=>img"]) && ($img = explode(",", $i))){ $tn = array_combine($img, $img); }else{
 		$tn = array($_GET['tn']=>preg_replace('/[^0-9a-z_]/', '', $_GET['tn']));
-	} $sql = "SELECT `". mpquot($_GET['fn'] ?: "img"). "` FROM {$conf['db']['prefix']}{$arg['modpath']}_{$tn[$_GET['tn']]} WHERE id=".(int)$_GET['id'];
+	} /*mpre($tn); exit;*/ $sql = "SELECT `". mpquot($_GET['fn'] ?: "img"). "` FROM {$conf['db']['prefix']}{$arg['modpath']}_{$tn[$_GET['tn']]} WHERE id=".(int)$_GET['id'];
 	$file_name = mpopendir("include")."/".($fn = mpql(mpqw($sql), 0, ($_GET['fn'] ?: "img")));
-	if(!array_search(array_pop(explode('.', $file_name)), array(1=>"jpg", "jpeg", "png", "gif"))){
-		$file_name = mpopendir("img/ext/". array_pop(explode('.', $file_name)). ".png");
-	}
+//	if(empty($fn)){ $file_name = mpopendir("modules/{$arg['modpath']}/img/no.png"); }
 }else{
 	$file_name = mpopendir("modules/{$arg['modpath']}/img/". basename($_GET['']));
-} header ("Content-type: image/". array_pop(explode('.', $file_name)));
+}
+header ("Content-type: image/". array_pop(explode('.', $file_name)));
 echo mprs($file_name, $_GET['w'], $_GET['h'], $_GET['c']);
 
 ?>
