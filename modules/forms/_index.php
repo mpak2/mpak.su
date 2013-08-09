e400fa372e30ddc7e37988fa74df3598fb55250e
mpak <github.com@mpak.su>
2013-08-09 08:10:03 +0000

    Fri Aug  9 08:10:03 UTC 2013

--- modules/forms/_index.php
+++ modules/forms/_index.php
@@ -1,43 +1,44 @@
 <? die;
 
 if($_GET['anket_id']){
-	$tpl['anket'] = mpql(mpqw("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_anket WHERE id=". (int)$_GET['anket_id']), 0);
-	$conf['index'] = mpql(mpqw("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_index WHERE id=". (int)$tpl['anket']['index_id']), 0);
+	$conf['tpl']['anket'] = mpql(mpqw("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_anket WHERE id=". (int)$_GET['anket_id']), 0);
+	$conf['index'] = mpql(mpqw("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_index WHERE id=". (int)$conf['tpl']['anket']['index_id']), 0);
 	if($conf['index']['uid'] == $conf['user']['uid']){
-		$tpl['result'] = mpqn(mpqw("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_result WHERE anket_id=". (int)$tpl['anket']['id']), 'vopros_id', 'variant_id');//	mpre($tpl['result']);
+		$conf['tpl']['result'] = mpqn(mpqw("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_result WHERE anket_id=". (int)$conf['tpl']['anket']['id']), 'vopros_id', 'variant_id');//	mpre($conf['tpl']['result']);
 	}else{
 		header("Location: /{$arg['modpath']}/{$conf['index']['id']}");
-	} $_GET['id'] = $tpl['anket']['index_id'];
+	} $_GET['id'] = $conf['tpl']['anket']['index_id'];
 }
 
-$tpl['index'] = qn("SELECT id.*, u.name AS uname FROM {$conf['db']['prefix']}{$arg['modpath']}_index AS id LEFT JOIN {$conf['db']['prefix']}users AS u ON id.uid=u.id WHERE id.id=". (int)$_GET['id']);
+if($_GET['id']){
+	$conf['tpl']['index'] = mpqn(mpqw("SELECT id.*, u.name AS uname FROM {$conf['db']['prefix']}{$arg['modpath']}_index AS id LEFT JOIN {$conf['db']['prefix']}users AS u ON id.uid=u.id WHERE id.id=". (int)$_GET['id']));
 
-if($index = $tpl['index'][ $_GET['id'] ]){
-	$tpl['type'] = qn("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_type");
+	$conf['tpl']['type'] = mpqn(mpqw("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_type"));
 
-	$tpl['vopros'] = mpqn(mpqw("SELECT v.id AS vid, v.* FROM {$conf['db']['prefix']}{$arg['modpath']}_index AS id LEFT JOIN {$conf['db']['prefix']}{$arg['modpath']}_vopros AS v ON id.id=v.index_id WHERE id.id=". (int)$_GET['id']. " ORDER BY v.type_id, v.sort, v.id"), 'type_id', 'vid');
-	$tpl['variant'] = mpqn(mpqw("SELECT vt.* FROM {$conf['db']['prefix']}{$arg['modpath']}_index AS id LEFT JOIN {$conf['db']['prefix']}{$arg['modpath']}_vopros AS v ON id.id=v.index_id LEFT JOIN {$conf['db']['prefix']}{$arg['modpath']}_variant AS vt ON v.id=vt.vopros_id WHERE id.id=". (int)$_GET['id']), 'vopros_id', 'id');
+	$conf['tpl']['vopros'] = mpqn(mpqw("SELECT v.id AS vid, v.* FROM {$conf['db']['prefix']}{$arg['modpath']}_index AS id LEFT JOIN {$conf['db']['prefix']}{$arg['modpath']}_vopros AS v ON id.id=v.index_id WHERE id.id=". (int)$_GET['id']. " ORDER BY v.type_id, v.sort, v.id"), 'type_id', 'vid');
 
-	if($anket = $tpl['index'][ $_GET['id'] ]){
-		foreach($tpl['vopros'] as $t){
+	$conf['tpl']['variant'] = mpqn(mpqw("SELECT vt.* FROM {$conf['db']['prefix']}{$arg['modpath']}_index AS id LEFT JOIN {$conf['db']['prefix']}{$arg['modpath']}_vopros AS v ON id.id=v.index_id LEFT JOIN {$conf['db']['prefix']}{$arg['modpath']}_variant AS vt ON v.id=vt.vopros_id WHERE id.id=". (int)$_GET['id']), 'vopros_id', 'id');
+
+	if($anket = $conf['tpl']['index'][ $_GET['id'] ]){
+		foreach($conf['tpl']['vopros'] as $t){
 			foreach($t as $v){
 				if(!empty($v['tn'])){
 					$fields = mpqn(mpqw("SHOW COLUMNS FROM `". mpquot($v['tn']). "`"), 'Field');
 					if(array_search($v['type'], array("radio", "select")) !== false){
-						$tpl['variant'][ $v['id'] ] = mpqn(mpqw("SELECT * FROM ". mpquot($v['tn']). " WHERE 1 ". ($fields['uid'] ? " AND uid=". (int)$conf['user']['uid'] : ""). " ORDER BY name"));
-						if($v['type'] != "radio") $tpl['variant'][ $v['id'] ] += array("0"=>array("name"=>""));
+						$conf['tpl']['variant'][ $v['id'] ] = mpqn(mpqw("SELECT * FROM ". mpquot($v['tn']). " WHERE 1 ". ($fields['uid'] ? " AND uid=". (int)$conf['user']['uid'] : ""). " ORDER BY name"));
+						if($v['type'] != "radio") $conf['tpl']['variant'][ $v['id'] ] += array("0"=>array("name"=>""));
 					}else if($v['type'] == "file"){
 						$key = implode("_", array_slice(explode("_", $anket['tn']), 2, 999)). "_id";
-						$tpl['variant'][ $v['id'] ] = mpqn(mpqw("SELECT * FROM ". mpquot($v['tn']). " WHERE 1 ". ($fields['uid'] ? " AND uid=". (int)$conf['user']['uid'] : ""). " AND ". mpquot($key). "=0 ORDER BY id"));
+						$conf['tpl']['variant'][ $v['id'] ] = mpqn(mpqw("SELECT * FROM ". mpquot($v['tn']). " WHERE 1 ". ($fields['uid'] ? " AND uid=". (int)$conf['user']['uid'] : ""). " AND ". mpquot($key). "=0 ORDER BY id"));
 					}else if($v['type'] == "check"){
-						$tpl['variant'][ $v['id'] ] = mpqn(mpqw("SELECT * FROM ". mpquot($v['tn']). " WHERE 1 ". ($fields['uid'] ? " AND uid=". (int)$conf['user']['uid'] : ""). " ORDER BY name"));
+						$conf['tpl']['variant'][ $v['id'] ] = mpqn(mpqw("SELECT * FROM ". mpquot($v['tn']). " WHERE 1 ". ($fields['uid'] ? " AND uid=". (int)$conf['user']['uid'] : ""). " ORDER BY name"));
 					}
 				}
 			}
-		}// mpre($tpl['variant']);
+		}// mpre($conf['tpl']['variant']);
 
 		if($_POST){
-			if(array_key_exists("null", $_GET) && ($vopros = $tpl['vopros'][0][ $_POST['vopros_id'] ])){
+			if(array_key_exists("null", $_GET) && ($vopros = $conf['tpl']['vopros'][0][ $_POST['vopros_id'] ])){
 				if($img_id = mpfid($vopros['tn'], $vopros['alias'], null, $_POST['vopros_id'], array('*'=>'*'))){
 	//				mpqw("UPDATE ". mpquot($vopros['tn']). " SET img=\"". mpquot($fn). "\" WHERE id=". (int)$insert_id);
 				} exit(json_encode(array("id"=>$img_id, "tn"=>explode("_", $vopros['tn'], 3))));
@@ -46,16 +47,16 @@ if($index = $tpl['index'][ $_GET['id'] ]){
 				$tpl['captcha'] = "false";
 			}else{
 				mpqw("INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_anket SET time=". time(). ", index_id=". (int)$_GET['id']. ", uid=". (int)$conf['user']['uid']);
-				if($tpl['anket_id'] = mysql_insert_id()){
-					foreach($tpl['vopros'] as $type_id=>$vopros){
+				if($conf['tpl']['anket_id'] = mysql_insert_id()){
+					foreach($conf['tpl']['vopros'] as $type_id=>$vopros){
 						foreach($vopros as $vopros_id=>$v){
 							if(($v['type'] == 'check') && !empty($_POST[ $vopros_id ])){
 								foreach($_POST[ $vopros_id ] as $variant_id=>$val){
-									mpqw($sql = "INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_result SET anket_id=". (int)$tpl['anket_id']. ", vopros_id=". (int)$vopros_id. ", variant_id=". (int)$variant_id);
+									mpqw($sql = "INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_result SET anket_id=". (int)$conf['tpl']['anket_id']. ", vopros_id=". (int)$vopros_id. ", variant_id=". (int)$variant_id);
 									$cds[$vopros_id][] = $variant_id;
 								}
 							}elseif($_POST[$vopros_id] && (($v['type'] == 'text') || ($v['type'] == 'textarea') || ($v['type'] == 'map'))){
-								mpqw($sql = "INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_result SET anket_id=". (int)$tpl['anket_id']. ", vopros_id=". (int)$vopros_id. ", val=\"". mpquot($_POST[$vopros_id]). "\"");
+								mpqw($sql = "INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_result SET anket_id=". (int)$conf['tpl']['anket_id']. ", vopros_id=". (int)$vopros_id. ", val=\"". mpquot($_POST[$vopros_id]). "\"");
 								$fds[ $v['alias'] ] = $_POST[$vopros_id];
 							}elseif(($v['type'] == 'file')){
 								if($v['tn']){ # Отдельная таблица для изображений
@@ -67,17 +68,17 @@ if($index = $tpl['index'][ $_GET['id'] ]){
 									}else{ /*mpre($_FILES);*/ }// mpre($fds);
 								}
 							}elseif((int)$_POST[ $vopros_id ]){
-								$sql = "INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_result SET anket_id=". (int)$tpl['anket_id']. ", vopros_id=". (int)$vopros_id. ", variant_id=". (int)$_POST[$vopros_id];
+								$sql = "INSERT INTO {$conf['db']['prefix']}{$arg['modpath']}_result SET anket_id=". (int)$conf['tpl']['anket_id']. ", vopros_id=". (int)$vopros_id. ", variant_id=". (int)$_POST[$vopros_id];
 								$fds[ $v['alias'] ] = (int)$_POST[$vopros_id];
 								mpqw($sql);
 							} $res[] = "\n#{$v['id']} {$v['name']} : ". $_POST[$vopros_id];
 						}
-					} mpevent("Заполнение формы {$index['name']}", $_SERVER['REQUERT_URI'], $anket['uid'], "/{$arg['modpath']}/anket_id:{$tpl['anket_id']}", implode("<br />", $res), $_POST, $conf['user'], $_SERVER);
+					} mpevent("Заполнение формы", $_SERVER['REQUERT_URI'], $anket['uid'], "/{$arg['modpath']}/anket_id:{$conf['tpl']['anket_id']}", implode("<br />", $res), $_POST);
 					if($fds){ # Свойства результирующей таблицы
 						$tpl['mysql_inset_id'] = mpfdk($anket['tn'], null, array("time"=>time(), "uid"=>$conf['user']['uid'])+(array)$fds);
 						if($fds_img){
 							foreach($fds_img as $v){
-								$vp = $tpl['vopros'][0][ $v ];
+								$vp = $conf['tpl']['vopros'][0][ $v ];
 								$key = implode("_", array_slice(explode("_", $anket['tn']), 2, 999)). "_id";
 								mpqw($sql = "UPDATE ". mpquot($vp['tn']). " SET ". mpquot($key). "=". (int)$tpl['mysql_inset_id']. " WHERE uid=". (int)$conf['user']['uid']. " AND ". mpquot($key). "=0");// mpre($sql);
 							}
@@ -91,7 +92,7 @@ if($index = $tpl['index'][ $_GET['id'] ]){
 								$fk .= "_id";
 							}
 							if(mpql(mpqw("SHOW TABLES WHERE Tables_in_{$conf['db']['name']}='". mpquot($tn). "'"), 0)){ # Есть ли таблица связанных элементов в базе данных
-								$pk = array_shift(array_slice(explode("_", $tpl['index'][ $_GET['id'] ]['tn'], 3), 2, 1));
+								$pk = array_shift(array_slice(explode("_", $conf['tpl']['index'][ $_GET['id'] ]['tn'], 3), 2, 1));
 								foreach($ds as $v){
 									mpqw("INSERT INTO {$tn} SET {$fk}=". (int)$v. ", {$pk}_id=". (int)$tpl['mysql_inset_id']);
 								}
@@ -107,7 +108,7 @@ if($index = $tpl['index'][ $_GET['id'] ]){
 		}
 	}
 }else{
-	$tpl['index'] = mpql(mpqw("SELECT o.*, u.name AS uname FROM {$conf['db']['prefix']}{$arg['modpath']}_index AS o LEFT JOIN {$conf['db']['prefix']}users AS u ON o.uid=u.id ORDER BY o.id DESC"));
+	$conf['tpl']['index'] = mpql(mpqw("SELECT o.*, u.name AS uname FROM {$conf['db']['prefix']}{$arg['modpath']}_index AS o LEFT JOIN {$conf['db']['prefix']}users AS u ON o.uid=u.id ORDER BY o.id DESC"));
 }
 
 ?>
