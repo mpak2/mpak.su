65b63e73715745df58bd9638c1e01652d0007146
mpak <github.com@mpak.su>
2013-08-24 22:32:07 +0000

    Добавлена отсылка уведомлений на смс

--- modules/.subscribe/sql.php
+++
@@ -1,9 +0,0 @@
-<? die;
-
-//DROP TABLE IF EXISTS `test`.`mp_subscribe_index`;
-//CREATE TABLE `test`.`mp_subscribe_index` SELECT * FROM `shop_mpak_su`.`mp_business_index` WHERE (description LIKE "%строитель%") LIMIT 10000
-
-// DROP TABLE IF EXISTS `argo_mpak_su`.`mp_subscribe_zakazrf`;
-// CREATE TABLE `argo_mpak_su`.`mp_subscribe_zakazrf` SELECT * FROM `shop_mpak_su`.`mp_subscribe_zakazrf` WHERE (addr LIKE "%ленинград%" OR addr LIKE "%петербург%") LIMIT 10000
-
-?>
--- modules/ymaps/delpoint.php
+++
@@ -1,6 +0,0 @@
-<? die;
-
-mpqw("DELETE FROM {$conf['db']['prefix']}{$arg['modpath']}_point WHERE uid=". (int)$conf['user']['uid']. " AND id=". (int)$_POST['id']);
-echo mysql_affected_rows() == 1 ? 'deleted' : 'Ошибка удаления';
-
-?>
