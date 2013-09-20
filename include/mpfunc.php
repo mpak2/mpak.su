d2822a2cee3ca376d5284a1625438ceb42e94ce4
mpak <github.com@mpak.su>
2013-09-20 13:40:12 +0000

    Fri Sep 20 13:40:10 UTC 2013

--- include/mpfunc.php
+++ include/mpfunc.php
@@ -1550,4 +1550,32 @@ function mprs($file_name, $max_width=0, $max_height=0, $crop=0){
 	}
 }
 
+if(!function_exists("array_column")){
+	function array_column(array $input, $columnKey, $indexKey = null) {
+		$result = array();
+		if(null === $indexKey){
+			if (null === $columnKey){
+				// trigger_error('What are you doing? Use array_values() instead!', E_USER_NOTICE);
+				$result = array_values($input);
+			}
+			else {
+				foreach ($input as $row){
+					$result[] = $row[$columnKey];
+				}
+			}
+		}else{
+			if (null === $columnKey){
+				foreach ($input as $row){
+					$result[$row[$indexKey]] = $row;
+				}
+			}
+			else {
+				foreach ($input as $row){
+					$result[$row[$indexKey]] = $row[$columnKey];
+				}
+			}
+		} return $result;
+	}
+}
+
 ?>
