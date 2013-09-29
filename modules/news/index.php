06220cbb4f20c49bcb4668cc73b8825e99e10761
mpak <github.com@mpak.su>
2013-09-29 17:20:01 +0000

    Sun Sep 29 17:20:01 UTC 2013

--- modules/news/index.php
+++ modules/news/index.php
@@ -1,25 +1,9 @@
 <? die;
 
-/*$sql = "SELECT SQL_CALC_FOUND_ROWS p.*, p.id AS id, CONCAT(SUBSTR(p.text, 1, 500)) as txt, u.name AS uname, k.name AS kname
-FROM {$conf['db']['prefix']}{$arg['modpath']}_post AS p
-LEFT JOIN {$conf['db']['prefix']}users AS u
-	ON p.uid=u.id
-LEFT JOIN {$conf['db']['prefix']}{$arg['modpath']}_cat AS k
-	ON p.cat_id=k.id WHERE 1=1". ($_GET['id'] ? " AND p.id=".(int)$_GET['id'] : "").($_GET['cat_id'] ? " AND p.cat_id=".(int)$_GET['cat_id'] : '');
-
-if (!$_GET['id']) $sql .= " ORDER BY time DESC LIMIT ". ((int)$_GET['p']*5). ",5";
-$conf['tpl']['news'] = mpql(mpqw($sql));
-
-if ((int)$_GET['id']){
-	mpqw("UPDATE {$conf['db']['prefix']}{$arg['modpath']}_post SET count=count+1 WHERE id=".(int)$_GET['id']." LIMIT 1");
-	$conf['settings']['title'] = $conf['tpl']['news'][0]['tema'];
-	$conf['tpl']['news'][0]['count']++;
-}else{
-	$conf['tpl']['mpager'] = mpager(mpql(mpqw("SELECT FOUND_ROWS() AS count"), 0, 'count')/5);
-}*/
-
 $tpl['cat'] = qn("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_cat");
-$tpl['index'] = qn("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_index ORDER BY time DESC");
+
+$tpl['index'] = qn("SELECT SQL_CALC_FOUND_ROWS * FROM {$conf['db']['prefix']}{$arg['modpath']}_index WHERE 1". ($_GET['id'] ? " AND id=". (int)$_GET['id'] : " ORDER BY time DESC"). " LIMIT ". ($_GET['p']*10). ",10");
+$tpl['mpager'] = mpager(ql("SELECT FOUND_ROWS()/10 AS cnt", 0, 'cnt'));
 
 if($n = $tpl['index'][ $_GET['id'] ] && !empty($n['count'])){
 	mpqw("UPDATE {$conf['db']['prefix']}{$arg['modpath']}_index SET count=count+1 WHERE id=". (int)$n['id']);
