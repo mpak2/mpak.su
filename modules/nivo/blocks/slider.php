771fc803e7628827547a1b169b913295771905e8
mpak <github.com@mpak.su>
2013-10-06 15:30:18 +0000

    Sun Oct  6 15:30:18 UTC 2013

--- modules/nivo/blocks/slider.php
+++ modules/nivo/blocks/slider.php
@@ -3,14 +3,17 @@
 
 //if(array_key_exists('blocks', $_GET['m']) && array_key_exists('null', $_GET) && ($_GET['id'] == $arg['blocknum']) && $_POST){};
 
-//$tpl['index'] = qn("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_{$arg['fn']}");
+$tpl['index'] = qn("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_index");
 
 ################################# верстка ################################# ?>
+<script src="/<?=$arg['modname']?>:jquery.nivo.slider/null/script.js"></script>
 <script>
 	$(function(){
-		$('#slider').data('nivoslider').start();
+		$('.nivo_index').nivoSlider();
 	});
 </script>
-<div class="nivo_index">
-	
+<div class="nivo_index" style="width:600px; height:400px;">
+	<? foreach($tpl['index'] as $index): ?>
+		<img src="/<?=$arg['modname']?>:img/<?=$index['id']?>/tn:index/fn:img/w:600/h:400/c:1/null/img.png">
+	<? endforeach; ?>
 </div>
