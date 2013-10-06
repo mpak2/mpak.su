7825e4694710d4d01d0cdf26bab9d947c840c93d
mpak <github.com@mpak.su>
2013-10-06 15:50:14 +0000

    Sun Oct  6 15:50:14 UTC 2013

--- modules/nivo/blocks/slider.php
+++ modules/nivo/blocks/slider.php
@@ -7,13 +7,14 @@ $tpl['index'] = qn("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_index
 
 ################################# верстка ################################# ?>
 <script src="/<?=$arg['modname']?>:jquery.nivo.slider/null/script.js"></script>
+<link href="/<?=$arg['modname']?>:nivo-slider/null/style.css" type="text/css" rel="stylesheet" />
 <script>
 	$(function(){
 		$('.nivo_index').nivoSlider();
 	});
 </script>
-<div class="nivo_index" style="width:600px; height:400px;">
+<div class="nivo_index" style="width:700px; height:400px;">
 	<? foreach($tpl['index'] as $index): ?>
-		<img src="/<?=$arg['modname']?>:img/<?=$index['id']?>/tn:index/fn:img/w:600/h:400/c:1/null/img.png">
+		<img src="/<?=$arg['modname']?>:img/<?=$index['id']?>/tn:index/fn:img/w:700/h:400/c:1/null/img.png">
 	<? endforeach; ?>
 </div>
