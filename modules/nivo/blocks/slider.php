2436e6b65d4ee867914ad5334e517ab1895dad53
mpak <github.com@mpak.su>
2013-10-06 15:20:17 +0000

    Sun Oct  6 15:20:16 UTC 2013

---
+++ modules/nivo/blocks/slider.php
@@ -0,0 +1,16 @@
+<? die; # Заголовка блока
+################################# php код #################################
+
+//if(array_key_exists('blocks', $_GET['m']) && array_key_exists('null', $_GET) && ($_GET['id'] == $arg['blocknum']) && $_POST){};
+
+//$tpl['index'] = qn("SELECT * FROM {$conf['db']['prefix']}{$arg['modpath']}_{$arg['fn']}");
+
+################################# верстка ################################# ?>
+<script>
+	$(function(){
+		$('#slider').data('nivoslider').start();
+	});
+</script>
+<div class="nivo_index">
+	
+</div>
