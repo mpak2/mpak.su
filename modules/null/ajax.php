cce6befe6c04183c61486460b1df4a449d828a93
mpak <github.com@mpak.su>
2013-10-18 20:40:05 +0000

    Fri Oct 18 20:40:04 UTC 2013

--- modules/null/ajax.php
+++ modules/null/ajax.php
@@ -1,13 +1,12 @@
 <? die;
 
 if(!empty($_REQUEST['class']) && $class = explode(" ", $_REQUEST['class'])){ # klesh запросы
-//mpre($_REQUEST);
-//mpre($_REQUEST['id']);
 	if($arg['access'] > 1){
 		mpevent("Аякс запрос /{$arg['modpath']}:{$class[0]}", $conf['user']['uid'], $_REQUEST);
-		if($class[1] == "debug") mpre($_REQUEST);
-		if(($_REQUEST['id'] < 0) && ($arg['access'] > 2)){
-			mpqw($sql = "DELETE FROM {$conf['db']['prefix']}{$arg['modpath']}_{$class[0]} WHERE 1". ($arg['access'] > 3 ? "" : " AND uid=". $conf['user']['uid']). " AND id=". (int)($_REQUEST['id']*-1)); exit($_REQUEST['id']);
+		if($class[1] == "debug"){
+			exit(mpre($_REQUEST));
+		}else if(($_REQUEST['id'] < 0) && ($arg['access'] > 2)){
+			mpqw($sql = "DELETE FROM {$conf['db']['prefix']}{$arg['modpath']}_{$class[0]} WHERE 1". ($arg['access'] > 3 ? "" : " AND uid=". $conf['user']['uid']). " AND id=". (int)($_REQUEST['id']*-1)); exit((string)0);
 		}else if(array_key_exists("val", $_REQUEST) && ($arg['access'] > 2)){
 			$klesh_id = mpfdk("{$conf['db']['prefix']}{$arg['modpath']}_{$class[0]}", $w = array("id"=>$_REQUEST["id"]), $w += array("{$class[1]}"=>$_REQUEST['val'])+$_REQUEST, $w); die((string)$klesh_id);
 		}else if(!array_key_exists("id", $_REQUEST)){
