a2fa75c078f395dde342601c72b6aa8b6e61c659
github.com@mpak.su <mpak>
2014-07-04 14:37:44 +0400

    Условие на подключение конфигурационного файла

--- include/config.php
+++ include/config.php
@@ -3,8 +3,10 @@
 mb_internal_encoding("UTF-8");
 date_default_timezone_set('Europe/Moscow');
 
-$conf['db']['open_basedir'] = ini_get("open_basedir");
-//$conf['db']['open_basedir'] = dirname(dirname(__FILE__)). ":". ini_get('upload_tmp_dir');
+if(!array_key_exists('open_basedir', (array)$conf['db'])){
+	$conf['db']['open_basedir'] = ini_get("open_basedir");
+//	$conf['db']['open_basedir'] = dirname(dirname(__FILE__)). ":". ini_get('upload_tmp_dir');
+}
 
 $conf['db']['conn'] = null;
 $conf['db']['type'] = 'mysql';
