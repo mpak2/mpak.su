0b9dc05b1e1cde1025ce0c65827f282b8e6f580d
github.com@mpak.su <mpak>
2014-11-07 19:39:08 +0400

    Удален мусор из файла конфигурации, обновлен архив

--- include/config.php
+++ include/config.php
@@ -3,15 +3,10 @@
 mb_internal_encoding("UTF-8");
 date_default_timezone_set('Europe/Moscow');
 
-if(!array_key_exists('open_basedir', (array)$conf['db'])){
-	$conf['db']['open_basedir'] = ini_get("open_basedir");
-//	$conf['db']['open_basedir'] = dirname(dirname(__FILE__)). ":". ini_get('upload_tmp_dir');
-}
-
 $conf['db']['conn'] = null;
 $conf['db']['type'] = 'mysql';
 $conf['db']['prefix'] = 'mp_';
 $conf['db']['host'] = 'localhost';
-$conf['db']['login'] = 'mpak.cms';
-$conf['db']['name'] = 'mpak.cms';
-$conf['db']['pass'] = 'password';
+$conf['db']['login'] = 'root';
+$conf['db']['name'] = 'mpak';
+$conf['db']['pass'] = '';
