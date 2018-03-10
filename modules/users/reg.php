b419a7b5f131a5542931bd2a21dd0a18bf85a061
mpak <mpak@localhost.localdomain>
2018-03-10 13:43:54 +0300

    Уведомления об ошибках регистрации

--- modules/users/reg.php
+++ modules/users/reg.php
@@ -1,17 +1,18 @@
 <?
 
 if(!$_POST){//mpre("Не аякс запрос");
-}elseif(!$name = mpquot(get($_POST, 'name'))){ mpre("Регистрационное имя не указано");
-}elseif(!$pass = get($_POST, 'pass')){ mpre("Пароль для регистрации не указан");
-}elseif(get($_POST, 'pass') != get($_POST, 'pass2')){ mpre("Пароли не совпадают");
-}elseif($users = rb("{$conf['db']['prefix']}users","name","[{$name}]")){ mpre("Пользователь уже зарегистрирован");
-}elseif(!$sess = get($conf, 'user', 'sess')){ mpre("Ошибка полученя сессии текущего пользователя");
-}elseif(!$mphash = mphash($name, $pass)){ mpre("Ошибка генерации пароля");
-}elseif(!$users = fk("{$conf['db']['prefix']}users", $w = array("name"=>$name), $w += array("type_id"=>1, "pass"=>$mphash, "reg_time"=>time(), "last_time"=>time(), "email"=>get($_POST, 'email'), "ref"=>get($conf, 'user', 'sess', 'ref'), "refer"=>get($conf, 'user', 'sess', 'refer')))){ mpre("Ошибка регистрации пользователя");
-}elseif(!$grp = get($conf, 'settings', 'user_grp')){ mpre("Ошибка определения пользовательской группы");
-}elseif(!$users_grp = rb("users-grp", "name",$w = "[{$grp}]")){ mpre("Ошибка выборки группы {$w}");
-}elseif(!$users_mem = fk("users-mem", $w = ["uid"=>$users['id'], "grp_id"=>$users_grp['id']], $w)){ mpre("Ошибка добавления пользователя `{$users["name"]}` в группу '{$users_grp["name"]}'");
-}elseif(!$sess = fk("{$conf['db']['prefix']}sess", ["id"=>$sess["id"]], null, ['uid'=>$users["id"]])){ mpre("Ошибка обновления сессии пользователя");
-}else{ mpevent("Регистрация нового пользователя", $name, $users['id'], $_POST);
+}elseif(!$name = mpquot(get($_POST, 'name'))){ die(!pre("Регистрационное имя не указано"));
+}elseif(!$pass = get($_POST, 'pass')){ die(!pre("Пароль для регистрации не указан"));
+}elseif(get($_POST, 'pass') != get($_POST, 'pass2')){ die(!pre("Пароли не совпадают"));
+}elseif($users = rb("{$conf['db']['prefix']}users","name","[{$name}]")){ die(!pre("Пользователь уже зарегистрирован"));
+}elseif(!$sess = get($conf, 'user', 'sess')){ die(!pre("Ошибка полученя сессии текущего пользователя"));
+}elseif(!$mphash = mphash($name, $pass)){ die(!pre("Ошибка генерации пароля"));
+}elseif(!$users = fk("{$conf['db']['prefix']}users", $w = array("name"=>$name), $w += array("type_id"=>1, "pass"=>$mphash, "reg_time"=>time(), "last_time"=>time(), "email"=>get($_POST, 'email'), "ref"=>get($conf, 'user', 'sess', 'ref'), "refer"=>get($conf, 'user', 'sess', 'refer')))){ die(!pre("Ошибка регистрации пользователя"));
+}elseif(!$grp = get($conf, 'settings', 'user_grp')){ die(!pre("Ошибка определения пользовательской группы"));
+}elseif(!$users_grp = rb("users-grp", "name",$w = "[{$grp}]")){ die(!pre("Ошибка выборки группы {$w}"));
+}elseif(!$users_mem = fk("users-mem", $w = ["uid"=>$users['id'], "grp_id"=>$users_grp['id']], $w)){ die(!pre("Ошибка добавления пользователя `{$users["name"]}` в группу '{$users_grp["name"]}'"));
+//}elseif(!$sess = fk("{$conf['db']['prefix']}sess", ["id"=>$sess["id"]], null, ['uid'=>$users["id"]])){ die(!pre("Ошибка обновления сессии пользователя", $sess));
+}else{// pre($sess);
+	mpevent("Регистрация нового пользователя", $name, $users['id'], $_POST);
 	exit(json_encode($users));
 }
