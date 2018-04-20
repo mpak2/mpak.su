ca3737bd0e1870fd0aa7022a4469266f84a01301
mpak2 <github.com@mpak.su>
2018-04-20 18:22:56 +0300

    Правка вывода ресурсов темы, формулировка ошибок

--- modules/themes/index.php
+++ modules/themes/index.php
@@ -1,6 +1,5 @@
 <?
 
-//if(!$file = (get($_GET, '') ?: get($_GET, 'q'))){ pre("Файл шаблона не задан", $_GET);
 if(!$file = call_user_func(function(){
 		if($file = get($_GET, 'q')){ return $file;
 		}elseif($file = get($_GET, '')){ return $file;
@@ -17,12 +16,11 @@ if(!$file = call_user_func(function(){
 }elseif(!$ext = last(explode('.', $file))){ pre("ОШИБКА определения расширения файла");
 }elseif(!include_once(mpopendir('modules/files/defaultmimes.php'))){ mpre("ОШИБКА подключения списка типов файлов");
 }elseif(!$type = (get($conf['defaultmimes'], $ext) ?: "text/$ext")){ mpre("ОШИБКА подулючения типа файла по расширению `{$ext}`");
-}elseif(!$res = mpopendir($res_name)){ mpre("ОШИБКА расчета полного пути до файла <b>{$res_name}</b>", $_GET);
+}elseif(!$res = mpopendir($res_name)){ mpre("ОШИБКА файл не найден <b>{$res_name}</b>", $_GET);
+	header("HTTP/1.0 404 Not Found");
+}elseif($f = fopen($res, "rb")){ mpre("ОШИБКА открытия файла на чтение `{$res}`");
 	header("HTTP/1.0 404 Not Found");
 }else{ header("Content-type: {$type}");
-			if($f = fopen($res, "rb")){
-				while(!feof($f)) {
-					echo fread($f, 256);
-				}
-			} exit();
-} header("HTTP/1.0 404 Not Found");
+	while(!feof($f)){ echo fread($f, 256); }
+	exit(fclose($f));
+}
