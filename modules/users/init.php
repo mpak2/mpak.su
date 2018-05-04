467d8da2467268f6fa04b815f85b8ac488081547
mpak2 <github.com@mpak.su>
2018-05-04 15:12:53 +0300

    Изменение структуры таблиц событий

--- modules/users/init.php
+++ modules/users/init.php
@@ -1,12 +1,16 @@
 <?
 
-echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}` (
+<? # modules/users/init.php
+
+if(mpsettings($t = "users", "Пользователи") && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type_id` int(11) NOT NULL,
   `refer` int(11) NOT NULL,
   `img` varchar(255) NOT NULL,
   `ref` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
   `pass` varchar(255) NOT NULL,
   `reg_time` int(11) NOT NULL,
   `last_time` int(11) NOT NULL,
@@ -18,50 +22,35 @@ echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}` (
   UNIQUE KEY `name` (`name`),
   KEY `ref` (`refer`),
   KEY `pass` (`pass`),
-  KEY `ref_2` (`ref`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8";
-mpqw($sql);
-
-echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}_event` (
+  KEY `ref_2` (`ref`),
+  KEY `type_id` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} if(mpsettings($t = "users_event", "События") && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `time` int(11) NOT NULL,
+  `up` int(11) NOT NULL COMMENT 'Последнее обновление события',
   `uid` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `count` int(11) NOT NULL,
-  `log` smallint(6) NOT NULL,
   `hide` smallint(6) NOT NULL,
-  `send` smallint(6) NOT NULL,
-  `limit` int(11) NOT NULL,
-  `cmail` int(11) NOT NULL,
-  `last` int(11) NOT NULL,
-  `max` int(11) NOT NULL,
-  `min` int(11) NOT NULL,
-  `log_last` int(11) DEFAULT NULL,
   `description` varchar(255) NOT NULL,
-  `subject` varchar(255) NOT NULL,
-  `text` text NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_2` (`name`),
   KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8";
-mpqw($sql);
-
-echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}_event_logs` (
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} if(mpsettings($t = "users_event_logs", "Журнал") && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `time` int(11) NOT NULL,
   `event_id` int(11) NOT NULL,
   `uid` int(11) NOT NULL,
   `description` text NOT NULL,
-  `own` varchar(255) NOT NULL,
-  `return` varchar(255) NOT NULL,
-  `zam` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `event_id` (`event_id`),
   KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8";
-mpqw($sql);
-
-echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}_event_mess` (
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} if(mpsettings($t = "users_event_mess", "Сообщения") && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `time` int(11) NOT NULL,
   `uid` int(11) NOT NULL,
@@ -73,10 +62,9 @@ echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}_event_
   PRIMARY KEY (`id`),
   KEY `time` (`time`),
   KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8";
-mpqw($sql);
-
-echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}_event_notice` (
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} if(mpsettings($t = "users_event_notice", "Уведомления") && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `time` int(11) NOT NULL,
   `uid` int(11) NOT NULL,
@@ -93,34 +81,84 @@ echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}_event_
   KEY `event_id` (`event_id`),
   KEY `uid` (`uid`),
   KEY `grp_id` (`grp_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8";
-mpqw($sql);
-
-echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}_grp` (
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} if(mpsettings($t = "users_event_params", "Параметры") && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` int(11) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} if(mpsettings($t = "users_event_value", "Значение") && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` int(11) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} if(mpsettings($t = "users_grp", "Группы") && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8";
-mpqw($sql);
-echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}_mem` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`uid` int(11) NOT NULL,
-	`grp_id` int(11) NOT NULL,
-	PRIMARY KEY (`id`),
-	UNIQUE KEY `uid` (`uid`,`grp_id`),
-	CONSTRAINT `mp_users_mem_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `mp_users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8";
-mpqw($sql);
-
-echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}_type` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-	`auth` int(11) NOT NULL,
-	`description` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8";
-mpqw($sql);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} if(mpsettings($t = "users_sess", null) && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `ref` varchar(255) NOT NULL,
+  `refer` int(11) NOT NULL,
+  `last_time` int(11) NOT NULL,
+  `count_time` int(11) NOT NULL,
+  `count` int(11) NOT NULL,
+  `cnull` int(11) NOT NULL,
+  `sess` varchar(255) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `agent` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `last_time` (`last_time`),
+  KEY `uid` (`uid`,`cnull`,`count`),
+  KEY `ip` (`ip`),
+  KEY `agent` (`agent`),
+  KEY `sess` (`sess`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} if(mpsettings($t = "users_type", "Типы") && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `auth` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} if(mpsettings($t = "users_event_values", "Значения") && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` int(11) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `event_logs_id` int(11) DEFAULT NULL,
+  `event_params_id` int(11) DEFAULT NULL,
+  `event_value_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `event_params_id` (`event_params_id`),
+  KEY `event_value_id` (`event_value_id`),
+  KEY `event_logs_id` (`event_logs_id`),
+  CONSTRAINT `mp_users_event_values_ibfk_1` FOREIGN KEY (`event_logs_id`) REFERENCES `mp_users_event_logs` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} if(mpsettings($t = "users_mem", "Участники") && !tables($table = ("{$conf['db']['prefix']}{$t}"))){
+qw("CREATE TABLE `{$table}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `grp_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uid` (`uid`,`grp_id`),
+  CONSTRAINT `mp_users_mem_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `mp_users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+}
 
 mpqw("INSERT INTO `{$conf['db']['prefix']}settings` (`modpath`, `name`, `value`, `aid`, `description`) VALUES ('{$arg['modpath']}', '{$arg['modpath']}', 'Пользователи', '4', '')");
 mpqw("INSERT INTO `{$conf['db']['prefix']}settings` (`modpath`, `name`, `value`, `aid`, `description`) VALUES ('{$arg['modpath']}', '{$arg['modpath']}_event', 'События', '4', '')");
@@ -151,30 +189,6 @@ mpqw("INSERT INTO `{$conf['db']['prefix']}settings` (`modpath`, `name`, `value`,
 mpqw("INSERT INTO `{$conf['db']['prefix']}settings` (`modpath`, `name`, `value`, `aid`, `description`) VALUES ('{$arg['modpath']}', '{$arg['modpath']}_activation', '', '0', 'Активация пользователя')");
 mpqw("INSERT INTO `{$conf['db']['prefix']}settings` (`modpath`, `name`, `value`, `aid`, `description`) VALUES ('{$arg['modpath']}', '{$arg['modpath']}_reg_text', 'Спасибо за регистрацию. Рады приветствовать вас на нашем сайте', '0', 'Текст при регистрации')");
 mpqw("INSERT INTO `{$conf['db']['prefix']}settings` (`modpath`, `name`, `value`, `aid`, `description`) VALUES ('{$arg['modpath']}', '{$arg['modpath']}_tpl_exceptions', '1', '0', 'Выпадающие списки')");
-
-echo '<p>'.$sql = "CREATE TABLE `{$conf['db']['prefix']}{$arg['modpath']}_sess` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
-  `ref` varchar(255) NOT NULL,
-  `refer` int(11) NOT NULL,
-  `last_time` int(11) NOT NULL,
-  `count_time` int(11) NOT NULL,
-  `count` int(11) NOT NULL,
-  `cnull` int(11) NOT NULL,
-  `sess` varchar(255) NOT NULL,
-  `ip` varchar(255) NOT NULL,
-  `agent` varchar(255) NOT NULL,
-  `url` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `last_time` (`last_time`),
-  KEY `uid` (`uid`,`cnull`,`count`),
-  KEY `ip` (`ip`),
-  KEY `agent` (`agent`),
-  KEY `sess` (`sess`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8";
-mpqw($sql);
-
-mpqw("INSERT INTO `{$conf['db']['prefix']}settings` (`modpath`, `name`, `value`, `aid`, `description`) VALUES ('{$arg['modpath']}', 'sess', 'Сессии', '1', 'Таблица сессий')");
 
 mpqw("INSERT INTO `{$conf['db']['prefix']}settings` (`modpath`, `name`, `value`, `aid`, `description`) VALUES ('{$arg['modpath']}', 'sess_time', '3600', '1', 'Время сессии')");
 mpqw("INSERT INTO `{$conf['db']['prefix']}settings` (`modpath`, `name`, `value`, `aid`, `description`) VALUES ('{$arg['modpath']}', 'del_sess', '0', '1', 'Отслеживание сессий')");
