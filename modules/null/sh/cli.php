595076032f4168f01302395d19e66567860d2d65
Кривошлыков Евгений <github.com@mpak.su>
2018-11-24 05:56:40 +0300

    Запуск консольного скрипта из админки

--- modules/null/sh/cli.php
+++ modules/null/sh/cli.php
@@ -1,8 +1,8 @@
 <?
 
 // Подключаем консоль
-if(call_user_func(function($conf = []){ // Подключение компонентов
-		if(get($conf, 'db', 'conn')){// mpre("Используем соединение основной системы");
+if(!$conf = call_user_func(function($conf = []){ // Подключение компонентов
+		if(array_key_exists('conf', $GLOBALS)){ $conf = $GLOBALS["conf"]; // mpre("Используем соединение основной системы");
 		}elseif(!$DIR = explode("/", __DIR__)){ print_r("Ошибка определения текущей директории");
 		}elseif(!$offset = array_search($d = "html", $DIR)){ print_r("Директория с проектом не найдена `{$d}`"); print_r($DIR);
 		}elseif(!$folder = implode("/", array_slice($DIR, 0, $offset+1))){ print_r("Ошибка выборка директории");
@@ -18,12 +18,12 @@ if(call_user_func(function($conf = []){ // Подключение компоне
 		}elseif(!$inc($f = "include/config.php")){ pre("Ошибка подключения `{$f}`");
 		}elseif(!$modpath = (isset($argv) ? basename(dirname(dirname(__FILE__))) : basename(dirname(__FILE__)))){ mpre("Ошибка вычисления имени модуля");
 		}elseif(!$arg = ['modpath'=>$modpath, "fn"=>implode(".", array_slice(explode(".", basename(__FILE__)), 0, -1))]){ mpre("Установка аргументов");
-		}elseif(!isset($argv)){ pre("Запуск из веб интерфейса");
+		}elseif(isset($argv)){ pre("Запуск из веб интерфейса");
+		}elseif(!$GLOBALS["conf"] = $conf){ mpre("ОШИБКА сохранеения данных в глобальной переменной");
 		}elseif(!$conf['db']['conn'] = conn()){ mpre("Ошибка подключения БД попробуйте установить `apt install php-sqlite3`");
 		}else{ mpre("Установлено соединение");
-		}
-	}, $conf)){ mpre("ОШИБКА подключения компонентов");
-}elseif(!is_array($argv = (isset($argv) ? $argv : array_merge([0=>basename(__FILE__)], (array)get($_GET, 'argv'))))){ mpre("ОШИБКА получения аргументов адреса");
+		} return $conf;
+	})){ mpre("ОШИБКА подключения компонентов");
 }elseif(!$argv = call_user_func(function($argv = ""){
 		if(array_key_exists('argv', $GLOBALS)){ $argv = $GLOBALS['argv'];
 		}elseif(!$argv = array_merge([0=>basename(__FILE__)], (array)get($_GET, 'argv'))){ mpre("ОШИБКА получения аргументов адреса");
@@ -33,29 +33,17 @@ if(call_user_func(function($conf = []){ // Подключение компоне
 }elseif(array_search($cmd["parse"] = "Парсинг", $cmd) == get($argv, 1)){// pre("Метод", get($argv, 1));
 	if(!include("phar://index.phar/include/class/simple_html_dom.php")){ mpre("ОШИБКА подключения класса simple_html_dom");
 	}elseif(!$html = new simple_html_dom()){ mpre("Ошибка создания обьекта парсера");
-	}elseif(!$data = file_get_contents($h = "https://vk.com/catalog.php")){ mpre("ОШИБКА загрузки страницы {$h}");
-	}elseif(!$html->load($data)){ mpre("ОШИБКА разбора дом обьектов странциы");
-	}elseif(!$LINK = $html->find('nav.subpage_link a')){ mpre("ОШИБКА выборки всех ссылок регионов");
-	}else{ mpre("Расчет окончен");
+	}elseif(!$VIDEO_SOURCES = rb("video-sources")){ mpre("ОШИБКА получения адресов видеостраниц");
+	}elseif(!array_map(function($video_sources) use($html){
+			if(!$href = get($video_sources, "href")){ mpre("ОШИБКА адрес страницы видео не задан");
+			}elseif(!$data = file_get_contents($h = $href)){ mpre("ОШИБКА загрузки страницы {$h}");
+			}elseif(!$html->load($data)){ mpre("ОШИБКА разбора дом обьектов странциы");
+			}elseif(!$items = $html->find('h3')){ mpre("ОШИБКА выборки всех видео страницы <a href='{$href}'>{$href}</a>");
+			}else{ mpre($items);
+				mpre($video_sources);
+			}
+		}, $VIDEO_SOURCES)){ mpre("ОШИБКА парсинга страниц");
+	}else{ mpre("Расчет окончен", $VIDEO_SOURCES);
 	}
-}else{
-	mpre("argv: `{$argv}`", $cmd);
-}
+}else{ mpre($argv, $cmd); }
 
-/*
-if(!empty($conf)){ mpre("Жираф уже подключен");
-}elseif(isset($argv)){ print_r("Запускаем не из консоли", $argv);
-}elseif(!chdir($dir = "/var/www/html")){ print_r("Ошибка установки корневой директории");
-}elseif(!$phar = "phar://{$dir}/index.phar"){ print_r("Установка пути до фара");
-}elseif(!include("{$phar}/include/mpfunc.php")){ mpre("Библиотека функций");
-}elseif(!include("{$phar}/include/config.php")){ mpre("Конфиг");
-}elseif(file_exists($config = "{$dir}/include/config.php") && !include($config)){ mpre("Параметры доступа к БД");
-}else{// define ('INCLUDE_PATH', 'phar:///var/www/html/index.phar');
-
-//	$conf['db']['conn'] = new PDO("{$conf['db']['type']}:host={$conf['db']['host']};dbname={$conf['db']['name']};charset=UTF8", $conf['db']['login'], $conf['db']['pass'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC));
-//	$arg['modpath'] = basename(dirname(dirname(__FILE__)));
-//	$conf['fs']['path'] = $dirname;
-
-//	unset($conf['db']['pass']); $conf['db']['sql'] = array();
-}
-*/
