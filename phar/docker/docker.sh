515db44c9368f97e1c495a7e496f8b3fa0447f60
morosmile <newmorosmile@gmail.com>
2019-02-18 19:22:50 +0300

    Исправил ошибку при создании таблицы(primary key), добавил порт 8080 в docker файлы

--- phar/docker/docker.sh
+++ phar/docker/docker.sh
@@ -1,3 +1,3 @@
 sudo docker run -d --name apache2 -p 80:80 -p 1022:22 apache2
-sudo docker run -d --name apache2 -v /var/www/html:/var/www/html -v /var/www/cache:/tmp/cache -p 80:80 -p 1022:22 -p 443:443 e6a54782ee7e
+sudo docker run -d --name apache2 -v /var/www/html:/var/www/html -v /var/www/cache:/tmp/cache -p 80:80 -p 8080:8080 -p 1022:22 -p 443:443 e6a54782ee7e
 # sudo docker exec -it apache2 /bin/bash
